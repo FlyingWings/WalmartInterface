3ced3d0c9defb1bceed78410fbf48032e99b10fb
Alex <591647088@qq.com>
2018-05-03 17:24:19 +0800

    add example for secret config

--- secret_config.php
+++ secret_config.php
@@ -1,3 +1,11 @@
 <?php
 
-// All the secret config contents
+
+// Example of secret config array
+$config_array = [
+    'customer_id'=>'example-customer-id',
+    'api_key'=>'eample_api_key',
+    'channel_type'=>'example_channel_type',
+    'id'=>'random-value',
+    'shop_name'=>"example-shop-name"
+];
